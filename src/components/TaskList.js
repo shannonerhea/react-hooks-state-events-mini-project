be80b28519f4ef39b89280bf9a4f49890d7029e7
shannone <shannone426@gmail.com>
2022-09-23 07:32:07 -0400

    this was hard

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,37 +1,20 @@
+
+
 import React from "react";
-import Task from "./Task";
+import Task from "./Task"
 
 function TaskList({ tasks, onDeleteTask }) {
   const taskList = tasks.map((task) => (
     <Task
-      key={task.text}
-      text={task.text}
-      category={task.category}
-      onDeleteTask={onDeleteTask}
+    key={task.text}
+    text={task.text}
+    category={task.category}
+    onDeleteTask={onDeleteTask}
     />
-  ));
-  return <div className="tasks">{taskList}</div>;
-}
+  ))
+
+
+return <div classNmae="tasks">{taskList}</div>
+ }
 
 export default TaskList;
-
-// import React from "react";
-// //import Task from "./Task"
-
-// function TaskList({ text, category, onDeleteTask }) {
-//   function handleClick() {
-//     onDeleteTask(text);
-//   }
-
-//   return (
-//     <div className="task">
-//       <div className="label">{category}</div>
-//       <div className="text">{text}</div>
-//       <button onClick={handleClick} className="delete">
-//         X
-//       </button>
-//     </div>
-//   );
-// }
-
-// export default TaskList;
